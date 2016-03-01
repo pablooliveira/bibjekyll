a114a449f67ef6526e4647299c060d0ce0099a06
Pablo de Oliveira <pablo@sifflez.org>
2016-03-01 18:01:25 +0100

    Update header

--- bibjekyll.rb
+++ bibjekyll.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2010-2013 Pablo de Oliveira Castro and contributors 
+# Copyright (c) 2010-2016 Pablo de Oliveira Castro and contributors 
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
